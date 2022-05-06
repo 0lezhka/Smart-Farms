7863a0ed274671512be07253ff098a515841593f
Oleh Kopylchak <OKopylchak@agileassets.com>
2022-05-06 16:45:55 +0300

    Initial commit

---
+++ IndicatorHistoryService/src/main/java/com/softserve/indicatorhistoryservice/IndicatorHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.indicatorhistoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicatorHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndicatorHistoryServiceApplication.class, args);
+    }
+
+}
---
+++ IndicatorHistoryService/src/test/java/com/softserve/indicatorhistoryservice/IndicatorHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.indicatorhistoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicatorHistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
